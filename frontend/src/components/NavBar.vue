cf8f3af3fd8c5a84ff5e3bc320e40df7dcbaebe1
salem-karim <salem.karim@icloud.com>
2025-10-08 16:05:34 +0200

    some styling

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,14 +2,12 @@
   <nav
     class="fixed top-0 left-0 w-full flex justify-between items-center p-4 shadow-md bg-emerald-950"
   >
-    <RouterLink to="/" class="flex items-center gap-2">
+    <RouterLink to="/" class="flex items-center gap-2 rounded-md">
       <img alt="Vue logo" src="@/assets/logo.svg" width="32" height="32" />
     </RouterLink>
 
     <div class="flex gap-4">
-      <RouterLink to="/login" class="px-3 py-1 border rounded hover:bg-gray-100 transition">
-        Login
-      </RouterLink>
+      <RouterLink to="/login" class="px-3 py-1 border rounded transition"> Login </RouterLink>
       <RouterLink to="/register" class="px-3 py-1 border rounded text-white transition">
         Register
       </RouterLink>
