38e2c792cb2478029fa861a1f847eff2e5691beb
salem-karim <salem.karim@icloud.com>
2025-10-08 16:01:20 +0200

    navbar should work

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav
+    class="fixed top-0 left-0 w-full flex justify-between items-center p-4 shadow-md bg-emerald-950"
+  >
+    <RouterLink to="/" class="flex items-center gap-2">
+      <img alt="Vue logo" src="@/assets/logo.svg" width="32" height="32" />
+    </RouterLink>
+
+    <div class="flex gap-4">
+      <RouterLink to="/login" class="px-3 py-1 border rounded hover:bg-gray-100 transition">
+        Login
+      </RouterLink>
+      <RouterLink to="/register" class="px-3 py-1 border rounded text-white transition">
+        Register
+      </RouterLink>
+    </div>
+  </nav>
+</template>
